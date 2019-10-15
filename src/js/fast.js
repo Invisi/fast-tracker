39e4dabd8a8aaaad6d2a9efbb7f16dc0738c57a4
Grenar.2819 <max-gw2@enten.space>
2019-10-15 11:42:06 +0200

    big code restructure

--- src/js/fast.js
+++ src/js/fast.js
@@ -54,7 +54,8 @@ let itemDifference = {};
 let farmedItems    = [];
 
 /******************************************/
-/************ Functions *******************/
+/********** Base Functions ****************/
+
 // Build Select, for easier adding new farms
 let setPossibleFarm = function() {
   let str2html = []
@@ -82,7 +83,7 @@ let setPossibleFarm = function() {
 
 // Because I'm too lazy to write it everytime
 let buildEndpoint = function(ep) {
-  return baseURL + ep + '?access_token=' + inputToken.value;
+  return baseURL + ep + '?lang=en&access_token=' + inputToken.value;
 }
 
 // Building html table
@@ -90,18 +91,18 @@ let buildHTML = function(chars) {
   // Performance: First init array
   let str2html = [];
 
+  // bank
+  str2html.push('<tr id="hl-bank"><td>Bank</td><td class="start apicheck"></td><td class="stop apicheck"></td></tr>');
+  // wallet
+  str2html.push('<tr id="hl-wallet"><td>Wallet</td><td class="start apicheck"></td><td class="stop apicheck"></td></tr>');
+  // materials
+  str2html.push('<tr id="hl-materials"><td>Materials</td><td class="start apicheck"></td><td class="stop apicheck"></td></tr>');
+
   // For every character build a wrapper
   // lowercase character name and replace whitespace
   for (var i = chars.length - 1; i >= 0; i--) {
-    str2html.push('<tr id="hl-' + chars[i].toLowerCase().replace(/\s/g, '-') + '"><td>' + chars[i] + '</td><td><i class="fas fa-spinner"></i></td><td><i class="fas fa-spinner"></i></td></tr>');
+    str2html.push('<tr id="hl-' + chars[i].toLowerCase().replace(/\s/g, '-') + '"><td>' + chars[i] + '</td><td class="start apicheck"></i></td><td class="stop apicheck"></i></td></tr>');
   }
-
-  // bank
-  str2html.push('<tr id="hl-bank"><td>Bank</td><td><i class="fas fa-spinner"></i></td><td><i class="fas fa-spinner"></i></td></tr>');
-  // wallet
-  str2html.push('<tr id="hl-wallet"><td>Wallet</td><td><i class="fas fa-spinner"></i></td><td><i class="fas fa-spinner"></i></td></tr>');
-  // materials
-  str2html.push('<tr id="hl-materials"><td>Materials</td><td><i class="fas fa-spinner"></i></td><td><i class="fas fa-spinner"></i></td></tr>');
 
   // Performance: Then insert joined array
   trackedEndpoints.innerHTML = str2html.join('');
@@ -119,7 +120,77 @@ let formHandler = function(e) {
   }
 }
 
+// Implement "human readable" time to Number
+Number.prototype.toHumanTimer = function () {
+  let sec_num = parseInt(this, 10); // don't forget the second param
+  let hours   = Math.floor(sec_num / 3600);
+  let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  let seconds = sec_num - (hours * 3600) - (minutes * 60);
+  let time = '';
 
+  if (hours === 1) {
+    time = time + hours + ' hour ';
+  } else if (hours >= 1) {
+    time = time + hours + ' hours ';
+  }
+  if (minutes === 1) {
+    time = time + minutes + ' minute ';
+  } else if (minutes >= 1) {
+    time = time + minutes + ' minutes ';
+  }
+  if (seconds === 1) {
+    time = time + seconds + ' second ';
+  } else if (seconds >= 1) {
+    time = time + seconds + ' seconds ';
+  }
+
+  // return hours+' hours '+minutes+' minutes and '+ seconds + ' seconds';
+  return time;
+}
+
+let getAPIData = function (action) {
+  let pms = []
+
+  for (var i = characterNames.length - 1; i >= 0; i--) {
+    pms.push(getInventory(characterNames[i]).then(function(charinv) {
+        // Add done indicator
+        let trc = document.querySelector('#hl-' + charinv[0].toLowerCase().replace(/\s/g, '-'));
+        trc.querySelector('td.apicheck.'+action).classList.add('done')
+        return charinv[1];
+      }));
+  }
+
+  pms.push(getBank().then(function(bank) {
+      // Add done indicator
+      let trbank = document.querySelector('#hl-bank');
+      trbank.querySelector('td.apicheck.'+action).classList.add('done')
+      return {
+        bank: bank
+      };
+    }));
+
+  pms.push(getMaterials().then(function(materials) {
+      // Add done indicator
+      let trmaterials = document.querySelector('#hl-materials');
+      trmaterials.querySelector('td.apicheck.'+action).classList.add('done')
+      return {
+        materials: materials
+      };
+    }));
+
+  pms.push(getWallet().then(async function(wallet) {
+      // Add done indicator
+      let trwallet = document.querySelector('#hl-wallet');
+      trwallet.querySelector('td.apicheck.'+action).classList.add('done')
+      return {
+        wallet: wallet
+      };
+    }));
+
+  return pms;
+}
+
+// Start tracking, getting initial values
 let startTracking = async function() {
   getTokenInfo()
   .then(getCharacterNames)
@@ -129,103 +200,25 @@ let startTracking = async function() {
     formTracking.querySelector('input[type="submit"]').value = 'Stop Farming';
     characterNames = chars;
 
-    let promises = [];
-
-    for (var i = chars.length - 1; i >= 0; i--) {
-      promises.push(getInventory(chars[i]).then(function(charinv) {
-          // invStartingValues.push(calcBagCount(charinv[1]));
-          // Remove spinner, add checked mark
-          let trc = document.querySelector('#hl-' + charinv[0].toLowerCase().replace(/\s/g, '-'));
-          trc.querySelectorAll('td')[1].innerHTML = '<i class="fas fa-check"></i>';
-          return charinv[1];
-        }));
-    }
-
-    promises.push(getBank().then(function(bank) {
-        // bankStartingValues = calcBankCount(bank);
-        // Remove spinner, add checked mark
-        let trbank = document.querySelector('#hl-bank');
-        trbank.querySelectorAll('td')[1].innerHTML = '<i class="fas fa-check"></i>';
-        return {
-          bank: bank
-        };
-      }));
-
-    promises.push(getMaterials().then(function(materials) {
-        // materialsStartingValues = calcMaterialsCount(materials);
-        // Remove spinner, add checked mark
-        let trmaterials = document.querySelector('#hl-materials');
-        trmaterials.querySelectorAll('td')[1].innerHTML = '<i class="fas fa-check"></i>';
-        return {
-          materials: materials
-        };
-      }));
-
-    promises.push(getWallet().then(async function(wallet) {
-        // walletStartingValues = await calcWalletCount(wallet);
-        // Remove spinner, add checked mark
-        let trwallet = document.querySelector('#hl-wallet');
-        trwallet.querySelectorAll('td')[1].innerHTML = '<i class="fas fa-check"></i>';
-        return {
-          wallet: wallet
-        };
-      }));
+    let promises = getAPIData('start');
 
     Promise.all(promises).then(function(e) {
       timerStart = Date.now();
-      timerInterval = setInterval(timer, 1000);
+      timerInterval = setInterval(fnTimer, 1000);
       itemStartCount = flattenItems(e);
     });
   });
 }
 
+// Stop Tracking, getting values
 let stopTracking = async function() {
-  let promises = [];
-
-  clearInterval(timerInterval);
-
+  // Calculate the time difference from start until now
   timeFarmed = (Date.now() - timerStart) / 1000;
 
-  // trackingForm.querySelector('input[type="submit"]').disabled;
-  for (var i = characterNames.length - 1; i >= 0; i--) {
-    promises.push(getInventory(characterNames[i]).then(function(charinv) {
-      // invStartingValues.push(calcBagCount(charinv[1]));
-      // Remove spinner, add checked mark
-      let trc = document.querySelector('#hl-' + charinv[0].toLowerCase().replace(/\s/g, '-'));
-      trc.querySelectorAll('td')[2].innerHTML = '<i class="fas fa-check"></i>';
-      return charinv[1];
-    }));
-  }
+  // Stop displaying time
+  clearInterval(timerInterval);
 
-  promises.push(getBank().then(function(bank) {
-    // bankStartingValues = calcBankCount(bank);
-    // Remove spinner, add checked mark
-    let trbank = document.querySelector('#hl-bank');
-    trbank.querySelectorAll('td')[2].innerHTML = '<i class="fas fa-check"></i>';
-    return {
-      bank: bank
-    };
-  }));
-
-  promises.push(getMaterials().then(function(materials) {
-    // materialsStartingValues = calcMaterialsCount(materials);
-    // Remove spinner, add checked mark
-    let trmaterials = document.querySelector('#hl-materials');
-    trmaterials.querySelectorAll('td')[2].innerHTML = '<i class="fas fa-check"></i>';
-    return {
-      materials: materials
-    };
-  }));
-
-  promises.push(getWallet().then(async function(wallet) {
-    // walletStartingValues = await calcWalletCount(wallet);
-    // Remove spinner, add checked mark
-    let trwallet = document.querySelector('#hl-wallet');
-    trwallet.querySelectorAll('td')[2].innerHTML = '<i class="fas fa-check"></i>';
-    return {
-      wallet: wallet
-    };
-  }));
+  let promises = getAPIData('stop');
 
   Promise.all(promises).then(function(e) {
     itemStopCount = flattenItems(e);
@@ -233,6 +226,9 @@ let stopTracking = async function() {
   .then(calcDifference)
   .then(displayFarmedItems);
 }
+
+/******************************************/
+/*********** API Functions ****************/
 
 // Check if all permissions needed are granted
 // If no errors, start getting characters
@@ -256,6 +252,17 @@ let getTokenInfo = async function() {
     document.querySelector('p.errors').innerHTML = str2html.join('');
     throw Error(str2html.join(''));
   }
+}
+
+// Get AccountInfo, only for account name, for contributors
+let getAccountInfo = async function () {
+  let ep = buildEndpoint(epAccount);
+
+  // Request
+  const response = await fetch(ep);
+  let account = await response.json();
+
+  return account;
 }
 
 // Getting all Characters from Account
@@ -319,6 +326,28 @@ let getWallet = async function() {
   return wallet;
 }
 
+// Get description of all currencies
+let getCurrencyDetails = async function () {
+  let cEp = baseURL + '/v2/currencies?ids=all&lang=en';
+
+  const cResponse = await fetch(cEp);
+  let details = cResponse.json();
+
+  return details;
+}
+
+// let getItemDetails = async function (ids) {
+//   let iEp = baseURL + '/v2/items?lang=en&ids=' + ids.join(',');
+//   const cResponse = await fetch(cEp);
+//   let details = cResponse.json();
+//   return details;
+// }
+
+/******************************************/
+/******** Calculate Functions *************/
+
+// Flatten the JSON objects from API
+//
 let flattenItems = function(rawData) {
   let itemCount = {};
   // let walletCount
@@ -371,6 +400,7 @@ let flattenItems = function(rawData) {
   return itemCount;
 }
 
+//
 let calcDifference = function() {
   let count = 0;
   for (var item in itemStartCount) {
@@ -392,26 +422,19 @@ let calcDifference = function() {
   }
 }
 
-let currencyDetails = async function () {
-  let cEp = baseURL + '/v2/currencies?ids=all&lang=en';
 
-  const cResponse = await fetch(cEp);
-  let details = cResponse.json();
 
-  return details;
+/******************************************/
+/********** Display Functions *************/
+
+// Function for Timer
+let fnTimer = function () {
+  var delta = Date.now() - timerStart; // milliseconds elapsed since start
+  document.querySelector('span.timer').innerHTML = 'Farming for ' + Math.floor(delta / 1000).toHumanTimer();
 }
 
-// let itemDetails = async function (ids) {
-//   let iEp = baseURL + '/v2/items?lang=en&ids=' + ids.join(',');
-//   const cResponse = await fetch(cEp);
-//   let details = cResponse.json();
-//   return details;
-// }
-//
-
-
 let displayCurrencies = async function () {
-  let cDetails = await currencyDetails();
+  let cDetails = await getCurrencyDetails();
   let str2html = [];
 
   for (var i = cDetails.length - 1; i >= 0; i--) {6
@@ -482,73 +505,33 @@ let displayItems = async function() {
   }
 }
 
-let getAccountInfo = async function () {
-    // TODO: Fix limit for API
-    let ep = buildEndpoint(epAccount);
+let displayFarmedItems = function() {
+  Promise.all([displayItems(),displayCurrencies(),getAccountInfo()])
+  .then(generateCSV)
+}
 
-    // Request
-    const response = await fetch(ep);
-    let account = await response.json();
+let generateCSV = function (args) {
+  let firstLine = [selPossibleFarms.value, args[2].name, timeFarmed].join(',')+'\n';
 
-    return account;
-  }
+  let timestampForFileName = new Date().toISOString();
 
-  let displayFarmedItems = function() {
-    Promise.all([displayItems(),displayCurrencies(),getAccountInfo()])
-    .then(generateCSV)
-  }
+  let csvHeaders = ['Item Name','ID', 'Count'].join(',')+'\n';
+  let itemArray2CSV = farmedItems.map(e => e.join(',')).join('\n');
 
-  let generateCSV = function (args) {
-    let date = new Date().toISOString();
-    let head = [possibleFarms.value, args[2].name, timeFarmed].join(',')+'\n'
-    let headers = ['item','id', 'count'].join(',')+'\n';
-    let arr2csv = farmedItems.map(e => e.join(",")).join("\n");
-    let csvContent = 'data:text/csv;charset=utf-8,'+head+headers+arr2csv;
-    let encodedUri = encodeURI(csvContent);
-    let link = document.createElement('a');
-    link.setAttribute('href', encodedUri);
-    link.setAttribute('download', possibleFarms.value+'_'+args[2].name +'_'+ date +'.csv');
+  // Concat everything
+  let csvContent = 'data:text/csv;charset=utf-8,' + firstLine + csvHeaders + itemArray2CSV;
+
+  let encodedUri = encodeURI(csvContent);
+  let link = document.createElement('a');
+  link.setAttribute('href', encodedUri);
+  link.setAttribute('download', selPossibleFarms.value+'_'+args[2].name +'_'+ timestampForFileName +'.csv');
   document.body.appendChild(link); // Required for FF
 
   link.click(); // This will download the data file named 'my_data.csv'.
 }
 
-let timer = function () {
-  var delta = Date.now() - timerStart; // milliseconds elapsed since start
-  document.querySelector('span.timer').innerHTML = 'Farming for ' + Math.floor(delta / 1000).toHumanTimer();
-}
-
-Number.prototype.toHumanTimer = function () {
-    let sec_num = parseInt(this, 10); // don't forget the second param
-    let hours   = Math.floor(sec_num / 3600);
-    let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    let seconds = sec_num - (hours * 3600) - (minutes * 60);
-    let time = '';
-    // if (hours   < 10) {hours   = "0"+hours;}
-    // if (minutes < 10) {minutes = "0"+minutes;}
-    // if (seconds < 10) {seconds = "0"+seconds;}
-    if (hours === 1) {
-      time = time + hours + ' hour ';
-    } else if (hours >= 1) {
-      time = time + hours + ' hours ';
-    }
-    if (minutes === 1) {
-      time = time + minutes + ' minute ';
-    } else if (minutes >= 1) {
-      time = time + minutes + ' minutes ';
-    }
-    if (seconds === 1) {
-      time = time + seconds + ' second ';
-    } else if (seconds >= 1) {
-      time = time + seconds + ' seconds ';
-    }
-
-    // return hours+' hours '+minutes+' minutes and '+ seconds + ' seconds';
-    return time;
-  }
-
-  /******************************************/
-  /*************** Event ********************/
-  formTracking.addEventListener('submit', formHandler);
+/******************************************/
+/*************** Event ********************/
+formTracking.addEventListener('submit', formHandler);
 
 
