30ce9ab9a91985aed21c96d2b75cdab0974751f4
Grenar.2819 <max-gw2@enten.space>
2019-10-15 11:34:58 +0200

    cleanup

--- src/js/fast.js
+++ src/js/fast.js
@@ -65,13 +65,14 @@ let buildHTML = function(chars) {
   trackedEndpoints.innerHTML = str2html.join('');
 }
 
+// Handle form sumbit
 let formHandler = function(e) {
   // Do not submit Form
   e.preventDefault();
 
-  if (e.target.querySelector('input[name="actionHandler"]').value === '0') {
+  if (actionHandler === 0) {
     startTracking();
-  } else if (e.target.querySelector('input[name="actionHandler"]').value === '1') {
+  } else if (actionHandler === 1) {
     stopTracking();
   }
 }
@@ -82,7 +83,7 @@ let startTracking = async function() {
   .then(getCharacterNames)
   .then(function(chars) {
 
-    formTracking.querySelector('input[name="actionHandler"]').value = 1;
+    actionHandler = 1
     formTracking.querySelector('input[type="submit"]').value = 'Stop Farming';
     characterNames = chars;
 
@@ -367,7 +368,6 @@ let currencyDetails = async function () {
 //
 
 
-// TODO: Styling of gold, currently displayed as copper with gold sprite
 let displayCurrencies = async function () {
   let cDetails = await currencyDetails();
   let str2html = [];
@@ -384,7 +384,7 @@ let displayCurrencies = async function () {
         if (itemDifference['w'+cDetails[i].id] < 0)
            neg = -1;
         let overallCopperABS = itemDifference['w'+cDetails[i].id];
-        // let coppeitemDifference['w'+cDetails[i].id]
+
         let gold = Math.floor(overallCopperABS/10000) * neg;
         let leftCopper = overallCopperABS % 10000;
         let silver = Math.floor(leftCopper/100) * neg;
@@ -398,13 +398,9 @@ let displayCurrencies = async function () {
         if(copper !== 0)
           str2html.push(copper + '<span class="sprite"><img src="img/copper.png" alt="c"></span>');
         str2html.push('</li>');
-
-
-
-      // str2html.push('<li>'+itemDifference['w'+cDetails[i].id]+'<span class="sprite"><img src="' + cDetails[i].icon + '" alt="' + cDetails[i].name + '"></span></li>')
-    } else {
-      str2html.push('<li>'+itemDifference['w'+cDetails[i].id]+'<span class="sprite"><img src="' + cDetails[i].icon + '" alt="' + cDetails[i].name + '"></span></li>')
-    }
+      } else {
+        str2html.push('<li>'+itemDifference['w'+cDetails[i].id]+'<span class="sprite"><img src="' + cDetails[i].icon + '" alt="' + cDetails[i].name + '"></span></li>')
+      }
     }
   }
 
